332f106c4665100c30a3214d8eb776429d644423
Leo <leolimaasantos@gmail.com>
2026-07-23 19:25:49 -0300

    use /home/leoARCH env variable instead of the user folder

--- bash/.config/bash/rc.sh
+++ bash/.config/bash/rc.sh
@@ -1,6 +1,6 @@
-source ~leoARCH/.config/bash/shell.sh
-source ~leoARCH/.config/bash/aliases.sh
-source ~leoARCH/.config/bash/functions.sh
-source ~leoARCH/.config/bash/init.sh
-source ~leoARCH/.config/bash/env.sh
-[[ $- == *i* ]] && bind -f ~leoARCH/.config/bash/inputrc.sh
+source "$HOME/.config/bash/shell.sh"
+source "$HOME/.config/bash/aliases.sh"
+source "$HOME/.config/bash/functions.sh"
+source "$HOME/.config/bash/init.sh"
+source "$HOME/.config/bash/env.sh"
+[[ $- == *i* ]] && bind -f "$HOME/.config/bash/inputrc.sh"
